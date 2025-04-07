422324c2a240e820b779d00bfdf1787a4e74c756
ivan_zarubin <vanya.zarubin2023@yandex.ru>
2025-04-07 13:03:59 +0300

    feat: Added a new function fileType.sh using ls command

--- fileType.sh
+++ fileType.sh
@@ -3,43 +3,57 @@
 # Type:         function
 # Input:        file -> files' name
 # Output:       result -> files' type
-# Description:  this finction defines files' type using command test ( [ ). If
+# DESCRIPTION:  this finction defines file's type using command ls -ld. If
 #               the file isn't a simple file, hard link file directory, symbol
-#               link, symbolic file, fifo file, block file, socket, files' type will define as
+#               link, symbolic file, fifo file, block file, socket, file's type will define as
 #               unknown and script will be stopped with an error. 
 #               Command stat returns using specified format (-c option) number
 #               of hard links (%h option). If number of hard links more than
 #               file is a hard link.
 #
-# Bags:         let the file be a symbolic link. Let also [ -f ... ] is in the
-#               first place. Then file will be difined as simple file.
-#               SOLUTION: move [ -L ... ] check in the first place
+# BAGS:         
+#
 #
 # ADD:          add exit with an error in main file, maybe flag or else?
 
 function fileType {
     local result
-    if [ -L ${file} ]; then
-        result="symbolic link file"
-    elif [ -f ${file} ]; then
-        result="simple file"
+    local type
 
-        # Check: hardlink file
-        if [ "$(stat -c %h ${file})" -gt 1 ]; then
-            result="hard link file"
-        fi
-    elif [ -d ${file} ]; then
-        result="directory"
-    elif [ -c ${file} ]; then
-        result="character file"
-    elif [ -p ${file} ]; then
-        result="fifo-file"
-    elif [ -b ${file} ]; then
-        result="block file"
-    elif [ -S ${file} ]; then
-        result="socket file"
-    else
-        echo "${file} has unknown type"
-        exit 1
-    fi
+    type=$(ls -ld ${file} | awk '{print $1}' | head -c 1)
+    case ${type} in
+        -)
+            local num_hlink
+            
+            num_hlink=$(stat -c %h "$file")
+            if [ ${num_hlink} -gt 1 ]; then
+                result="hard link file"
+            else
+                result="simple file"
+            fi
+            ;;
+        d)
+            result="directory file"
+            ;;
+        l)
+            result="symbolic link file"
+            ;;
+        b)
+            result="block file"
+            ;;
+        c)
+            result="character file"
+            ;;
+        s)
+            result="socket file"
+            ;;
+        p)
+            result="fifo file"
+            ;;
+        *)
+            echo "${file} has unknown type"
+            exit 1
+            ;;
+    esac
     echo ${result}
+}
