5f0989ea02b7afa669b868cc3d79282c4df14117
ivan_zarubin <vanya.zarubin2023@yandex.ru>
2025-04-07 09:49:25 +0300

    build: Added new function (fileType.sh) for defining file's type using ls command

---
+++ fileType.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+
+# Type:         function
+# Input:        FILE -> files' name
+# Output:       result -> files' type
+# DESCRIPTION:  this finction defines file's type using command ls -ld. If
+#               the file isn't a simple file, hard link file directory, symbol
+#               link, symbolic file, fifo file, block file, socket, file's type will define as
+#               unknown and script will be stopped with an error. 
+#               Command stat returns using specified format (-c option) number
+#               of hard links (%h option). If number of hard links more than
+#               file is a hard link.
+#
+# BAGS:         
+#
+#
+# ADD:          add exit with an error in main file, maybe flag or else?
+
+function fileType {
+    local result
+    local type
+
+    type=$(ls -ld ${FILE} | awk '{print $1}' | head -c 1)
+    case ${type} in
+        -)
+            local num_hlink
+            
+            num_hlink=$(stat -c %h "$FILE")
+            if [ ${num_hlink} -gt 1 ]; then
+                result="hard link file"
+            else
+                result="simple file"
+            fi
+            ;;
+        d)
+            result="directory file"
+            ;;
+        l)
+            result="symbolic link file"
+            ;;
+        b)
+            result="block file"
+            ;;
+        c)
+            result="character file"
+            ;;
+        s)
+            result="socket file"
+            ;;
+        p)
+            result="fifo file"
+            ;;
+        *)
+            echo "${FILE} has unknown type"
+            exit 1
+            ;;
+    esac
+    echo ${result}
+}
